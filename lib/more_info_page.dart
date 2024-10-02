7e5d5736c5001f96d629964c3acbcdd9d124e296
Aditya Vasipalli <vasipalliaditya@gmail.com>
2024-10-02 21:54:00 +0530

    Update more_info_page.dart

--- lib/more_info_page.dart
+++ lib/more_info_page.dart
@@ -1,18 +1,37 @@
+// more_info_page.dart
 import 'package:flutter/material.dart';
 
 class MoreInfoPage extends StatelessWidget {
   final String title;
+  final String detailedInfo; // Add a field for the detailed info
 
-  const MoreInfoPage({Key? key, required this.title}) : super(key: key);
+  const MoreInfoPage({super.key, required this.title, required this.detailedInfo});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('More About $title'),
+        title: Text(title),
+        backgroundColor: Colors.black,
+        foregroundColor: Colors.red,
       ),
-      body: Center(
-        child: Text('More information about $title will go here.'),
+      backgroundColor: const Color.fromARGB(255, 223, 198, 229),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              title,
+              style: const TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 16),
+            Text(
+              detailedInfo, // Display the detailed information here
+              style: const TextStyle(fontSize: 18),
+            ),
+          ],
+        ),
       ),
     );
   }
